ab333c732d150834c36b2c242766232ffb4ba89f
Ahmedelsayed55 <am1096922@gmail.com>
2025-10-29 18:48:31 +0300

    v-fanAnimation

--- src/Fann.jsx
+++ src/Fann.jsx
@@ -1,28 +1,53 @@
-import React from 'react'
-import fan from './assets/Fan.png'
-import { motion } from 'motion/react';
+import React, { useState } from "react";
+import fan from "./assets/Fan.png";
+import { motion } from "motion/react";
 export default function Fann() {
-  const [isAnimating, setIsAnimating] = React.useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [fanSpeed, setFanSpeed] = useState(0);
+  const increment = () => {
+    if (isAnimating && fanSpeed >= 0 && fanSpeed < 3) {
+      setFanSpeed(fanSpeed + 1);
+    } else {
+      setFanSpeed(0);
+    }
+  };
+  const toggleAnimation = () => {
+    if (isAnimating) {
+      setIsAnimating(false);
+      setFanSpeed(0);
+    } else {
+      setIsAnimating(true);
+    }
+  }
+
   return (
-       <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <motion.img
         src={fan}
         alt=""
-        className="w-40 h-40"
-        animate={isAnimating ? { rotate: 360 } : { rotate: 0 }}
+        className="w-2/12 "
+        animate={{ rotate: isAnimating && fanSpeed >= 1 ? 360 * fanSpeed : 0  }}
         transition={{
-          duration: 0.5,
+          duration: fanSpeed === 3 ? 0.2 : fanSpeed === 2 ? 0.3 : 1,
           ease: "linear",
-          repeat: isAnimating ? Infinity : 0,
+          repeat: isAnimating && fanSpeed >= 1 ? Infinity : 0,
         }}
       />
-
+      <h1>
+        {fanSpeed} {isAnimating ? "on" : "off"}
+      </h1>
       <button
-        onClick={() => setIsAnimating(!isAnimating)}
+        className="mt-6 bg-amber-400 px-5 py-2 rounded-xl"
+        onClick={increment}
+      >
+        Fan Speed
+      </button>
+      <button
+        onClick={toggleAnimation}
         className="mt-6 bg-amber-400 px-5 py-2 rounded-xl"
       >
-        {isAnimating ? "إيقاف" : "تشغيل"}
+        {isAnimating ? "End" : "Start"}
       </button>
     </div>
-  )
+  );
 }
